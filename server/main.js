d73e12413f1a72f00d860caea697ba657b1a3dd5
Nuno Maia <nunofmaia@gmail.com>
2014-02-04 15:36:31 +0000

    Add user profile
    Show relevant information about the logged in user

--- server/main.js
+++ server/main.js
@@ -7,6 +7,16 @@ Meteor.methods({
       Fenix.setCode(code);
       var person = Fenix.getPerson();
       var user = Meteor.users.findOne({username: person.username });
+      _.extend(person, {
+        points: 0,
+        ranking: 0,
+        questions: [],
+        answers: [],
+        courses: [],
+        followed_questions: [],
+        badges: []
+      });
+
       if (!user) {
         Accounts.createUser({
           username: person.username,
