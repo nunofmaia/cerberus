72dce4b67440be9cf6d62e764a2d470a11b3790b
Nuno Maia <nunofmaia@gmail.com>
2014-02-04 17:30:24 +0000

    Add courses
    Fill database with all courses and retrieve user courses as well

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 Meteor.startup(function() {
     Fenix = new FenixClient();
+    if (Courses.find().count() === 0) {
+      processCourses();
+    }
 });
 
 Meteor.methods({
@@ -7,15 +10,24 @@ Meteor.methods({
       Fenix.setCode(code);
       var person = Fenix.getPerson();
       var user = Meteor.users.findOne({username: person.username });
+      var courses = Fenix.getCourses().enrolments;
+      var userCourses = [];
+      _.map(courses, function(course) {
+        var c = Courses.findOne({ name: course.name });
+        if (c) {
+          userCourses.push(c._id);
+        }
       _.extend(person, {
         points: 0,
         ranking: 0,
         questions: [],
         answers: [],
-        courses: [],
+        courses: userCourses,
         followed_questions: [],
         badges: []
       });
+
+      person.courses = userCourses;
 
       if (!user) {
         Accounts.createUser({
@@ -29,3 +41,24 @@ Meteor.methods({
     return person.username;
 }
 });
+
+function processCourses() {
+  var degrees = Fenix.getDegrees();
+  var degrees_courses = {};
+  _.map(degrees, function(degree) {
+    degrees_courses[degree.id] = Fenix.getDegreeCourses(degree.id);
+  });
+
+  var courses = _.chain(degrees_courses).values().flatten().uniq(function(item) {
+    return item.id;
+  }).value();
+
+  _.map(courses, function(course) {
+    Courses.insert({
+      id: course.id,
+      name: course.name,
+      acronym: course.acronym
+    });
+  });
+    console.log('courses done');
+}
