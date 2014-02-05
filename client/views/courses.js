cb6def5c432a64e201d2efc87f3deccad7a6e97f
Hugo Correia <hfcorreia91@gmail.com>
2014-02-05 19:15:38 +0000

    Update courses page to link to question creation

--- client/views/courses.js
+++ client/views/courses.js
@@ -1,8 +1,5 @@
 Template.courses.helpers({
     courses: function() {
-        return Courses.find();
-    },
-    userCourses: function() {
         var user = Meteor.user();
         if (user) {
             return Courses.find({ _id: { $in: user.profile.courses }});
