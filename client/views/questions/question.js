0ebc44042284366c618b4ccc802ea7a75f0ebb16
Hugo Correia <hfcorreia91@gmail.com>
2014-02-05 17:29:36 +0000

    Change questions' creation to be relative to a course

--- client/views/questions/question.js
+++ client/views/questions/question.js
@@ -1,5 +1,8 @@
 Template.question.helpers({
     authorName : function() {
         return Meteor.users.findOne(this.authorId).username;
+    },
+    courseName : function() {
+        return Courses.findOne( { _id : this.courseId  }).name;
     }
 });
