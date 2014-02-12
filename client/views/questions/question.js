28feb313f2e730b958b07e5a94b8394a4ff296ba
Nuno Maia <nunofmaia@gmail.com>
2014-02-12 13:31:53 +0000

    Follow and unfollow questions

--- client/views/questions/question.js
+++ client/views/questions/question.js
@@ -4,5 +4,25 @@ Template.question.helpers({
     },
     courseName : function() {
         return Courses.findOne( { _id : this.courseId  }).name;
+    },
+    followed: function() {
+    	var id = this._id;
+    	var found = _.find(Meteor.user().profile.followed_questions, function(questionID) {
+    		console.log(questionID, id);
+    		return questionID === id;
+    	});
+
+    	console.log(found);
+
+    	return found;
     }
+});
+
+Template.question.events({
+	'click #follow-question': function(e, t) {
+		Meteor.call('followQuestion', this._id);
+	},
+	'click #unfollow-question': function(e, t) {
+		Meteor.call('unfollowQuestion', this._id);
+	}
 });
