116dfd5832dc227a10e745ac6f7faf9961cb6402
Filipa Correia <filipaincorreia@gmail.com>
2014-02-14 17:29:55 +0000

    Add special acronym without weird numbers to courses.

---
+++ client/views/application/menu.js
@@ -0,0 +1,8 @@
+Template.menu.helpers({
+    courses: function() {
+        var user = Meteor.user();
+        if (user) {
+            return Courses.find({ id: { $in: user.profile.courses }});
+        }
+    }
+});
